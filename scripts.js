51dbbd04c6735af2fa607dcc322033f84b6e88bf
Anjinari Stephenson <aastephenson@atlassian.com>
2020-11-01 23:11:17 -0500

    Final

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+
+window.onload=response;
+
+function response(){
+    var subscribe = document.getElementsByTagName("button")[0];
+    subscribe.addEventListener("click", function(e){
+        var check  = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        var email = document.getElementById("email").value;
+        if(email.match(check)){
+            document.getElementsByClassName("message")[0].innerHTML = `Thank you! Your email address ${email} has been added to our mailing list!`;
+         }
+        else{
+            document.getElementsByClassName("message")[0].innerHTML = "Please enter a valid email address";
+        }
+        e.preventDefault();
+    }); 
+}
